08e42cd867c7c22e5af8f7594322aa828c33b142
Eray Hanoğlu <e.hanoglu@panates.com>
2025-02-28 14:23:58 +0300

    refactor: Refactored redisess module

---
+++ packages/redisess/src/types.ts
@@ -0,0 +1,25 @@
+import type { Logger } from '@nestjs/common';
+import type { ModuleMetadata } from '@nestjs/common/interfaces';
+import type { InjectionToken } from '@nestjs/common/interfaces/modules/injection-token.interface';
+import type { Cluster, Redis } from 'ioredis';
+import type { SessionManager } from 'redisess';
+
+export interface RedisessSessionOptions extends SessionManager.Options {
+  client: Redis | Cluster;
+}
+
+interface BaseModuleOptions {
+  token?: InjectionToken;
+  envPrefix?: string;
+  logger?: Logger | string;
+  global?: boolean;
+}
+
+export interface RedisessModuleOptions extends BaseModuleOptions {
+  useValue: RedisessSessionOptions;
+}
+
+export interface RedisessModuleAsyncOptions extends BaseModuleOptions, Pick<ModuleMetadata, 'imports'> {
+  useFactory?: (...args: any[]) => Promise<RedisessSessionOptions> | RedisessSessionOptions;
+  inject?: any[];
+}
